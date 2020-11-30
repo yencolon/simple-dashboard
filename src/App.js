f4608e5093af5803664690b10087ecff72aa8148
Yen Colon <yendejesusc41@gmail.com>
2020-11-29 20:50:59 -0400

    applying prettier

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
+import Login from "./views/Login";
+
 function App() {
-  return (
-    <div class="grid grid-cols-3 gap-2 place-content-center h-48">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-    </div>
-    // <div className="bg-green-200">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+  return <Login />
 }
 
 export default App;
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"],
+  purge: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
